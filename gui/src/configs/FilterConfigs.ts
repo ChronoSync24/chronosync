cadb679925ec2972252af67daf7c577b3a0eefcc
kfejzic1 <kfejzic1@etf.unsa.ba>
2025-07-21 20:26:23 +0200

    refactoring

---
+++ gui/src/configs/FilterConfigs.ts
@@ -0,0 +1,10 @@
+import { FilterField } from '../components/Filters';
+
+export const appointmentTypeFilterFields: FilterField[] = [
+  {
+    key: 'name',
+    label: 'Name',
+    type: 'text',
+    placeholder: 'Search by name...',
+  },
+];
