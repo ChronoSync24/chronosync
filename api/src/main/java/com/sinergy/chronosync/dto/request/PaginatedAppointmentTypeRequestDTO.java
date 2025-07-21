46520369341aae98008d6b23646c6967d4ccb8dc
kfejzic1 <kfejzic1@etf.unsa.ba>
2025-07-21 20:37:47 +0200

    paginated appointment type request dto implementation

---
+++ api/src/main/java/com/sinergy/chronosync/dto/request/PaginatedAppointmentTypeRequestDTO.java
@@ -0,0 +1,14 @@
+package com.sinergy.chronosync.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class PaginatedAppointmentTypeRequestDTO extends BasePaginationRequest {
+    private String name;
+}
