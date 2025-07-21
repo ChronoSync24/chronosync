ecec15f6e24e98aef545e5794891beb558bca288
kfejzic1 <kfejzic1@etf.unsa.ba>
2025-07-21 20:37:27 +0200

    explicit jwt signing algorithm

--- api/src/main/java/com/sinergy/chronosync/util/JwtUtils.java
+++ api/src/main/java/com/sinergy/chronosync/util/JwtUtils.java
@@ -78,7 +78,7 @@ public class JwtUtils {
 			.subject(userDetails.getUsername())
 			.issuedAt(new Date(System.currentTimeMillis()))
 			.expiration(new Date(System.currentTimeMillis() + 1000 * 3600 * JWT_EXPIRATION))
-			.signWith(getSigningKey())
+			.signWith(getSigningKey(), Jwts.SIG.HS256)
 			.compact();
 	}
 
