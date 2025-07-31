a7db821e3c29ab0a89c1be3e3fab3aa33db51803
kfejzic1 <kfejzic1@etf.unsa.ba>
2025-07-31 18:10:48 +0200

    crud backend implementation

--- api/src/main/java/com/sinergy/chronosync/service/ClientService.java
+++ api/src/main/java/com/sinergy/chronosync/service/ClientService.java
@@ -1,9 +1,9 @@
 package com.sinergy.chronosync.service;
 
 import com.sinergy.chronosync.dto.request.ClientRequestDTO;
+import com.sinergy.chronosync.dto.request.PaginatedClientRequestDTO;
 import com.sinergy.chronosync.model.Client;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 
 /**
  * Client service interface class.
@@ -15,7 +15,7 @@ public interface ClientService {
 	 *
 	 * @return {@link Page} of {@link Client} containing all clients for the user's firm
 	 */
-	Page<Client> getClients(PageRequest pageRequest);
+	Page<Client> getClients(PaginatedClientRequestDTO pageRequest);
 
 	/**
 	 * Creates a new client.
